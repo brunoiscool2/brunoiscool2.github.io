bc01ab3c40ffd27f574d4dabcc69a49654748a3b
ELLESFCK09 <84726294+brunoiscool2@users.noreply.github.com>
2025-03-31 10:55:27 -0400

    Update iframe.js

--- assets/js/iframe.js
+++ assets/js/iframe.js
@@ -4,41 +4,18 @@ const urlParam = getQueryParam('url');
 if (urlParam) {
     const decodedUrl = decodeURIComponent(urlParam);
     iframe.src = decodedUrl;
-    setCookie('iframeSrc', decodedUrl, 365); 
     history.replaceState(null, '', window.location.pathname);
     console.log('Iframe URL set from query param:', decodedUrl);
 } else {
-    const storedUrl = getCookie('iframeSrc');
-    iframe.src = storedUrl || 'https://example.com';
-    console.log(storedUrl ? 'Iframe URL set from cookies:' : 'Iframe URL set to default:', iframe.src);
+    iframe.src = 'https://example.com';
+    console.log('Iframe URL set to default:', iframe.src);
 }
 
 function getQueryParam(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-}
-
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = `${name}=${value}${expires}; path=/`;
-}
-
-function getCookie(name) {
-    const cookies = document.cookie.split('; ');
-    for (let cookie of cookies) {
-        const [key, value] = cookie.split('=');
-        if (key === name) return value;
-    }
-    return null;
+    return new URLSearchParams(window.location.search).get(param);
 }
 
 function toggleFullscreen() {
-    const iframe = document.getElementById('gameframe');
     if (iframe.requestFullscreen) {
         iframe.requestFullscreen();
     } else if (iframe.mozRequestFullScreen) {
@@ -51,24 +28,18 @@ function toggleFullscreen() {
 }
 
 function refreshIframe() {
-    const iframe = document.getElementById('gameframe');
     iframe.src = iframe.src;
     console.log('Iframe refreshed');
 }
 
 function goBack() {
-    if (document.referrer) {
-        window.location.href = document.referrer;
-    } else {
-        window.location.href = '/';
-    }
+    window.location.href = document.referrer || '/';
 }
 
 function activateChat() {
     const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3';
     script.async = true;
-    script.defer = true;
     document.body.appendChild(script);
     new Crate({
         server: '1271606448878780478',
