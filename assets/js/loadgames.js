62898bfbe23ec3435b603ab9c93bd0d4d3edebef
ELLESFCK09 <84726294+brunoiscool2@users.noreply.github.com>
2025-03-31 10:52:23 -0400

    Update loadgames.js

--- assets/js/loadgames.js
+++ assets/js/loadgames.js
@@ -1,28 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let gamesData = []; 
+    let gamesData = [];
     
     fetch('/assets/js/games.json')
         .then(response => response.json())
         .then(data => {
-            gamesData = data.games; 
-            renderGames(gamesData); 
-            
-
+            gamesData = data.games;
+            renderGames(gamesData);
             document.getElementById('searchBar').addEventListener('input', filterGames);
         })
         .catch(error => console.error('Error loading games:', error));
 
     function renderGames(games) {
         const gamesContainer = document.getElementById('games-container');
-        gamesContainer.innerHTML = ''; 
+        gamesContainer.innerHTML = '';
         
         games.forEach(game => {
             const gameElement = document.createElement('div');
             gameElement.className = 'game-box';
             gameElement.innerHTML = `
                 <a href="${game.url}">
-                    <div class="loader"></div> 
-                    <img src="${game.image}" alt="${game.name}" width="170" height="170">
+                    <div class="loader"></div>
+                    <img src="" alt="${game.name}" width="170" height="170" style="opacity: 0;">
                     <div class="game-title">${game.name}</div>
                 </a>
             `;
@@ -30,9 +28,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const img = gameElement.querySelector("img");
             const loader = gameElement.querySelector(".loader");
 
-            img.onload = function() {
-                loader.remove(); 
-                img.style.opacity = "1"; 
+            const tempImg = new Image();
+            tempImg.src = game.image;
+            tempImg.onload = function() {
+                img.src = game.image;
+                loader.remove();
+                img.style.opacity = "1";
             };
 
             gamesContainer.appendChild(gameElement);
