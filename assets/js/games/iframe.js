c292277650e0009f609a89ed138b615c1f933d25
ELLESFCK09 <84726294+brunoiscool2@users.noreply.github.com>
2025-04-03 16:22:11 -0400

    Delete assets/js/games directory

--- assets/js/games/iframe.js
+++
@@ -1,8 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-        const gameUrl = params.get("url");
-
-        if (gameUrl) {
-            document.getElementById("game-frame").src = gameUrl;
-        } else {
-            document.getElementById("game-frame").innerHTML = "No game selected.";
-        }
